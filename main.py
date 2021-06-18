5debe0e8ceb7bf480ec621ccf9be6d8393156520
Ryan Guide <ryan.guide@comcast.net>
2021-06-18 18:05:50 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.secret_key = "Bad key lol"
+Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template("login.html", form=form)
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    login = SubmitField('Login')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
